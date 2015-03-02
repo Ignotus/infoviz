7d215d4d93a50e6f3c3c51062c0d0eb98f706ca1
li-xiaoli <li.xiaoli@aliyun.com>
2015-03-03 00:21:22 +0100

    a very beginning step
    - js for panel on the right to come
    - a very beginning layout for “dashboard”
    - some css…
    - the main-min.js is not in use for now

---
+++ src/static/js/main.js
@@ -0,0 +1,44 @@
+// 
+// Map API (mapbox.com) doc:
+// https://www.mapbox.com/mapbox.js/example/v1.0.0/clicks-in-popups/
+  
+flag = 1; 
+
+
+// ----------------  show map, please change here --------------- // 
+L.mapbox.accessToken = 'pk.eyJ1IjoieGlhb2xpIiwiYSI6IkhpWkZhZFkifQ.RgWs4kq33jfD3d46_TTd6g';
+var map = L.mapbox.map('map', 'examples.map-i86nkdio')
+	    .setView([52.3648367,4.9151507], 13);  
+
+var marker = L.marker([52.3648367,4.9151507], {
+	      icon: L.mapbox.marker.icon({
+	        'marker-color': '#9c89cc'
+	      })
+	    })
+	    .bindPopup('<p>Halloooo~</p>')
+	    .addTo(map);
+ 
+//add a function to the element on map...
+marker.on('click', function(e) {
+	showHideBoard();
+});
+
+
+//////////////////////////////////////////////
+
+
+//call this function to show and hide the panel/board on the right of screen
+var showHideBoard = function(){
+	if(flag){
+        $('.board').animate({"margin-right": '-=300'});
+    }else{
+        $('.board').animate({"margin-right": '+=300'});
+    }
+    console.log(flag);
+    flag = flag?0:1;
+}
+ 
+
+
+
+
